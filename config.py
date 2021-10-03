1918bcb6c09e24c7076e6bb14a4fe0f66bec1c33
williamdubberley <william.dubberley@sesamesoftware.com>
2021-10-03 05:46:05 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,18 @@
 error_file = 'error_summary.txt'
 correction_file = 'corrections.sql'
+primary_key_file = 'primary_keys.sql'
 count_file = 'count.sql'
 path_to_logs = 'C:/Users/william/Downloads/BMClog'
 
 varchar_size = 4000
 connection = {
     'db1': {
-        'host': 'host',
-        'database': 'database',
-        'user': 'user',
-        'password': 'password'
+        'host': '192.168.1.72',
+        'database': 'sesamedb',
+        'user': 'sesame',
+        'password': 'R3lati0nal',
+        'schema': 'sesame',
+        'rj_config': 'BMCPG1toPSRMPDB'
     }
 }
 
