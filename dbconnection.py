0371b11c1e68a169520429b520ab9e5620cbaeb8
williamdubberley <william.dubberley@sesamesoftware.com>
2021-10-03 05:46:53 -0500

    Update dbconnection.py

--- dbconnection.py
+++ dbconnection.py
@@ -1,4 +1,5 @@
 import psycopg2
+
 import config
 
 
@@ -14,13 +15,6 @@ def table_exists(con, table_str):
     finally:
         cur.close()
     return exists
-
-
-def fetch_table_counts(dbname):
-    pass
-
-def fetch_table_count(conn,table_name):
-    pass
 
 
 def get_table_col_names(con, table_str):
@@ -53,13 +47,11 @@ def get_table_col_type(con, table_name, column_name):
 
 
 def get_postgresql_connection(database):
-    config.connection[database]['user'],
-    conn = psycopg2.connect(
+    return psycopg2.connect(
         host=config.connection[database]['host'],
         database=config.connection[database]['database'],
         user=config.connection[database]['user'],
         password=config.connection[database]['password'])
-    return conn
 
 
 def is_clob(con, table_name, column_name):
@@ -69,3 +61,34 @@ def is_clob(con, table_name, column_name):
     else:
         return False
 
+
+def get_length(conn, table_name, column_name):
+    sql = f'select max(length({column_name})) from {table_name};'
+    cur = conn.cursor()
+    cur.execute(sql)
+    max_field_length = cur.fetchone()
+    print(max_field_length)
+    return max_field_length
+
+
+def get_constraints(conn, table, schema):
+    cur = conn.cursor()
+    cur.execute(f"""SELECT pg_get_constraintdef(con.oid)
+                   FROM pg_catalog.pg_constraint con
+                        INNER JOIN pg_catalog.pg_class rel
+                                   ON rel.oid = con.conrelid
+                        INNER JOIN pg_catalog.pg_namespace nsp
+                                   ON nsp.oid = connamespace
+                   WHERE nsp.nspname = '{schema}'
+                         AND rel.relname = '{table}';""")
+    return cur.fetchall()
+
+
+def get_table_names(con, schema):
+    table_sql = f'''SELECT table_name FROM
+        information_schema.tables
+        WHERE
+        table_schema = '{schema}';'''
+    cur = con.cursor()
+    cur.execute(table_sql)
+    return cur.fetchall()
